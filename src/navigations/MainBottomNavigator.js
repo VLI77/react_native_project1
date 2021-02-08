d2301385966ecfda2ff1a1d538fab9c548ec19a0
VLI77 <vincent.li@hetic.net>
2021-02-08 13:54:48 +0100

    icon

--- src/navigations/MainBottomNavigator.js
+++ src/navigations/MainBottomNavigator.js
@@ -3,31 +3,44 @@ import {Text, View, Image, StyleSheet, ScrollView, Button, Linking, Animated} fr
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {SearchAndDetailNavigator} from "./SearchAndDetailNavigator.js";
 import HomeScreen from "../screens/HomeScreen";
+import { FontAwesome } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createBottomTabNavigator();
 
 export const MainBottomNavigator = () => {
     return (
-        <TabNavigator.Navigator     tabBarOptions={{
-            activeTintColor: '#B5A90F',
-            inactiveTintColor: 'lightgray',
-            activeBackgroundColor: '#B00020',
-            inactiveBackgroundColor: '#B00020',
-                style: {
-                      backgroundColor: '#B5A90F',
-                      paddingBottom: 3
-                }
-         }} >
-            <TabNavigator.Screen style={styles.navigator} name="HomeScreen" component={HomeScreen} />
+        
+        <TabNavigator.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'HomeScreen') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'ios-search' : 'ios-search-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#B5A90F',
+          inactiveTintColor: 'white',
+          activeBackgroundColor:'#B00020',
+          inactiveBackgroundColor: '#B00020',
+
+        }}
+      >
+            <TabNavigator.Screen name="HomeScreen" component={HomeScreen} />
+
             <TabNavigator.Screen name="Search" component={SearchAndDetailNavigator} />
         </TabNavigator.Navigator>
     )
 }
 
-const styles = StyleSheet.create({
-    navigator: {
 
-        backgroundColor: "red",
-    },
-  
-})
