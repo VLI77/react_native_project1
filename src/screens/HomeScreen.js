c89d8cdf68ae86d66ee5db1cfa8a45ffe10f5ab9
VLI77 <vincent.li@hetic.net>
2021-02-09 11:48:27 +0100

    push2

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HomepageButton from '../components/HomepageButton';
+import React, { useState, useEffect} from 'react';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import {HomepageButton} from '../components/HomepageButton';
 import {Logo} from '../components/Logo';
 import{getType} from '../services/movie'
 
